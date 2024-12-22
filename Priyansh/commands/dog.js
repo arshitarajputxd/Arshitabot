928ebe2d07ba9195199cd3a6b7cce2c117490226
arshitarajputxd <brandedqueenxd@gmail.com>
2024-12-22 22:08:40 +0530

    Update dog.js

--- Priyansh/commands/dog.js
+++ Priyansh/commands/dog.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "dog",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝐀𝐫𝐬𝐡𝐢𝐭𝐚 𝐑𝐚𝐣𝐩𝐮𝐭",
 	description: "Xem Boss",
 	commandCategory: "Picture",
 	usages: "dog [Text]",
