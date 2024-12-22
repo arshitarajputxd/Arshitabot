6e211c36d5461b481debc436037d34162c7518bc
arshitarajputxd <brandedqueenxd@gmail.com>
2024-12-22 22:07:07 +0530

    Update delete.js

--- Priyansh/commands/delete.js
+++ Priyansh/commands/delete.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "delete",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝐀𝐫𝐬𝐡𝐢𝐭𝐚 𝐑𝐚𝐣𝐩𝐮𝐭",
 	description: "Delete 🖱️",
 	commandCategory: "Image",
 	usages: "rank",
