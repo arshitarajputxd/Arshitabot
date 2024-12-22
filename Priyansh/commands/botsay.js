c3acbcbb125d96e2b3957a3b4120e678c2813b9a
arshitarajputxd <brandedqueenxd@gmail.com>
2024-12-22 21:27:27 +0530

    Update botsay.js

--- Priyansh/commands/botsay.js
+++ Priyansh/commands/botsay.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "bot-say",
 	version: "1.1.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝐀𝐫𝐬𝐡𝐢𝐭𝐚 𝐑𝐚𝐣𝐩𝐮𝐭",
 	description: "Bot Saying",
 	commandCategory: "ai",
 	usages: "[text/message/chat]",
