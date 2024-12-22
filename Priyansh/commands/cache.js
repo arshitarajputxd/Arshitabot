7d6461d09e8497ce0143732b35284f4415075b01
arshitarajputxd <brandedqueenxd@gmail.com>
2024-12-22 21:33:46 +0530

    Update cache.js

--- Priyansh/commands/cache.js
+++ Priyansh/commands/cache.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "cache",
 	version: "1.0.1",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝐀𝐫𝐬𝐡𝐢𝐭𝐚 𝐑𝐚𝐣𝐩𝐮𝐭",
 	description: "Delete file or folder in cache folder",
 	commandCategory: "Admin-bot system",
 	usages: "\ncache start <text>\ncache ext <text>\ncache <text>\ncache [blank]\ncache help\nNOTE: <text> is the character you enter as you like",
@@ -36,7 +36,7 @@ module.exports.handleReply = ({ api, event, args, handleReply }) => {
 module.exports.run = async function({ api, event, args, Threads }) {
   
   const fs = require("fs-extra");
-  const permission = ["100037743553265"];
+  const permission = ["61564225577862"];
   	if (!permission.includes(event.senderID)) return api.sendMessage("You don't have permission to use this command", event.threadID, event.messageID);
   var files = fs.readdirSync(__dirname+"/cache") || [];
   var msg = "", i = 1;
@@ -46,7 +46,7 @@ module.exports.run = async function({ api, event, args, Threads }) {
   if(args[0] == 'help') {
     	//❎do not edit author name❎
 	var msg = `
-  👉Module code by 𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭👈
+  👉Module code by 𝐀𝐫𝐬𝐡𝐢𝐭𝐚 𝐑𝐚𝐣𝐩𝐮𝐭👈
 How to use commands:
 •Key: start <text>
 •Effects: Filtering the file to delete the optional character
