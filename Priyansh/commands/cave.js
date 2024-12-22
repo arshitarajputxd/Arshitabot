c3fc375414aaab9776d03aa3ff1286f6f41a94ab
arshitarajputxd <brandedqueenxd@gmail.com>
2024-12-22 21:52:29 +0530

    Update cave.js

--- Priyansh/commands/cave.js
+++ Priyansh/commands/cave.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "cave",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝐀𝐫𝐬𝐡𝐢𝐭𝐚 𝐑𝐚𝐣𝐩𝐮𝐭",
 	description: "Sell ​​your own capital",
 	commandCategory: "Make money",
     cooldowns: 5,
