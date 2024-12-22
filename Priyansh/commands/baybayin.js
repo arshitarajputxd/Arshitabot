8ed4bc6c6d17239da20c94ed308f1b26fd23acf3
arshitarajputxd <brandedqueenxd@gmail.com>
2024-12-22 21:06:18 +0530

    Update baybayin.js

--- Priyansh/commands/baybayin.js
+++ Priyansh/commands/baybayin.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "baybayin",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝐀𝐫𝐬𝐡𝐢𝐭𝐚 𝐑𝐚𝐣𝐩𝐮𝐭",
 	description: "convert text to baybayin",
 	commandCategory: "Phương tiện",
 	usages: "baybayin [text]",
