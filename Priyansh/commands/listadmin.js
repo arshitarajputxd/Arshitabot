a719aaf052a42e04f4fd54ed4000f0896a3fad97
arshitarajputxd <brandedqueenxd@gmail.com>
2024-12-23 23:36:26 +0530

    Update listadmin.js

--- Priyansh/commands/listadmin.js
+++ Priyansh/commands/listadmin.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "listadmin",
     version: '1.0.0',
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝐀𝐫𝐬𝐡𝐢𝐭𝐚 𝐑𝐚𝐣𝐩𝐮𝐭",
     description: "List of group administrators",
     commandCategory: "Box Chat",
     usages: "dsqtv",
