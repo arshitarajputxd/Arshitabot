a9086319b725f0e3e684a17f9feae2625b1112ac
arshitarajputxd <brandedqueenxd@gmail.com>
2024-12-23 23:33:02 +0530

    Update japan.js

--- Priyansh/commands/japan.js
+++ Priyansh/commands/japan.js
@@ -2,7 +2,7 @@
   name: "japan",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "𝐀𝐫𝐬𝐡𝐢𝐭𝐚 𝐑𝐚𝐣𝐩𝐮𝐭",
   description: "Random ảnh girl",
   commandCategory: "random-img",
   usages: "japan",
