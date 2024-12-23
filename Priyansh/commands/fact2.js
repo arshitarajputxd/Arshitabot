003ad7ff82a38b8a1ea0284177f485c2317c892b
arshitarajputxd <brandedqueenxd@gmail.com>
2024-12-23 23:02:38 +0530

    Update fact2.js

--- Priyansh/commands/fact2.js
+++ Priyansh/commands/fact2.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "fact2",
     version: "1.0.1",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝐀𝐫𝐬𝐡𝐢𝐭𝐚 𝐑𝐚𝐣𝐩𝐮𝐭",
     description: "",
     commandCategory: "image",
     usages: "text",
