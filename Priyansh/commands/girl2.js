68c29a2e529b879825842d46a233cfc870f53a48
arshitarajputxd <brandedqueenxd@gmail.com>
2024-12-23 23:15:43 +0530

    Update girl2.js

--- Priyansh/commands/girl2.js
+++ Priyansh/commands/girl2.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "girl2",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "𝐀𝐫𝐬𝐡𝐢𝐭𝐚 𝐑𝐚𝐣𝐩𝐮𝐭",
   description: "Random image of girl",
   commandCategory: "random-img",
   usages: "girl2",
