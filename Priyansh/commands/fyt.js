40de735cb1b116ad2a0433ac47d92edf36fa535d
arshitarajputxd <brandedqueenxd@gmail.com>
2024-12-23 23:12:38 +0530

    Update fyt.js

--- Priyansh/commands/fyt.js
+++ Priyansh/commands/fyt.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "fyt",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝐀𝐫𝐬𝐡𝐢𝐭𝐚 𝐑𝐚𝐣𝐩𝐮𝐭",
     description: "War In Chatbox",
     commandCategory: "wargroup",
     usages: "[fyt]",
