bb68f41f0451907b830850a40d38892a471d1b38
arshitarajputxd <brandedqueenxd@gmail.com>
2024-12-23 23:44:29 +0530

    Update logout.js

--- Priyansh/commands/logout.js
+++ Priyansh/commands/logout.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "logout",
     version: "1.0.1",
     hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝐀𝐫𝐬𝐡𝐢𝐭𝐚 𝐑𝐚𝐣𝐩𝐮𝐭",
     description: "Logout ACC Bot",
     commandCategory: "System",
     usages: "",
