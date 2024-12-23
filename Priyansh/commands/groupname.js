548fb7e43fb6235f9bd9e38a5d3573d0598699bc
arshitarajputxd <brandedqueenxd@gmail.com>
2024-12-23 23:18:13 +0530

    Update groupname.js

--- Priyansh/commands/groupname.js
+++ Priyansh/commands/groupname.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "groupname",
 	version: "1.0.0", 
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝐀𝐫𝐬𝐡𝐢𝐭𝐚 𝐑𝐚𝐣𝐩𝐮𝐭",
 	description: "Rename your group",
 	commandCategory: "Box", 
 	usages: "groupname [name]", 
