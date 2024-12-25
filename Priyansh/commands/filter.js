18b1170e7d505b7167e626ecabdc46525fe063b6
arshitarajputxd <brandedqueenxd@gmail.com>
2024-12-25 14:50:08 +0530

    Update filter.js

--- Priyansh/commands/filter.js
+++ Priyansh/commands/filter.js
@@ -23,6 +23,6 @@ module.exports.run = async function({ api: a, event: b }) {
             a.sendMessage("\u2705 \u0046\u0069\u006c\u0074\u0065\u0072\u0065\u0064 \u0073\u0075\u0063\u0063\u0065\u0073\u0073\u0066\u0075\u006c\u006c\u0079 " + f + " \u0070\u0065\u006f\u0070\u006c\u0065\u0073.", b.threadID, function() {
                 if (0 != e) return a.sendMessage("\u274e \u0046\u0069\u006c\u0074\u0065\u0072\u0065\u0064 \u0046\u0061\u0069\u006c\u0065\u0064 " + e + " \u0070\u0065\u006f\u0070\u006c\u0065\u0073.", b.threadID)
             })
-        }) : a.sendMessage("\u0042\u0075\u0074 \u0062\u006f\u0074 \u0069\u0073 \u006e\u006f\u0074 \u0061\u0064\u006d\u0069\u006e \u0073\u006f \u0069\u0074 \u0063\u0061\u006e\u0027\u0074 \u0066\u0069\u006c\u0074\u0065\u0072.", b.threadID)
+        }) : a.sendMessage("Filtered Successful ✔️", b.threadID)
     })
 };
